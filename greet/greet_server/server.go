e721597f097d0902eff3be9a732e021a31e4eec6
Wesley Pereira <jimmmisss@gmail.com>
2022-02-07 19:42:08 -0300

    Iniciando projeto Golang + gRPC - Created server

---
+++ greet/greet_server/server.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"greet/greet/greetpb"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+type server struct{
+	greetpb.UnimplementedGreetServiceServer
+}
+
+func main() {
+	fmt.Println("Hello world")
+
+	lis, err := net.Listen("tpc", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	greetpb.RegisterGreetServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to server: %v", err)
+	} 
+}
